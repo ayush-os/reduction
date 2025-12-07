5ee1b7e1b8b9c42e27c8c6c5b75a150d9b074373
ayush <99374392+ayush-os@users.noreply.github.com>
2025-12-06 16:09:49 -0800

    tune 4 again

--- src/host_main.cu
+++ src/host_main.cu
@@ -73,8 +73,8 @@ int main() {
     checkCudaError(cudaMemset(d_output, 0, sizeof(float)), "d_output reset");
     checkCudaError(cudaEventRecord(start), "cudaEventRecord start");
 
-    reduce<<<1536, 512>>>(d_input, d_temp, N);
-    reduce<<<1, 256>>>(d_temp, d_output, 1536);
+    reduce<<<512, 512>>>(d_input, d_temp, N);
+    reduce<<<1, 256>>>(d_temp, d_output, 512);
 
     checkCudaError(cudaEventRecord(stop), "cudaEventRecord stop");
     checkCudaError(cudaEventSynchronize(stop), "cudaEventSynchronize stop");
